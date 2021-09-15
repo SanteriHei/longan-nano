aec3cfde9bba07a6bb53ba7b0f454ef05aeb9c9f
Santeri Heiskanen <heiskanens98@gmail.com>
2021-09-15 18:35:34 +0300

    Remove lcd feature, and make it a permament part of the crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 pub use gd32vf103xx_hal as hal;
-
-#[cfg(feature="lcd")]
-#[cfg_attr(docsrs, doc(cfg(feature = "lcd")))]
 pub mod lcd;
-
 pub mod led;
 pub mod stdout;
 
